050ba7da16cea2b0a31666a29c1ad2a8d748cd0e
eambru <eambrus033@gmail.com>
2024-07-07 19:23:03 +0200

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+//PascalCasing = capitalize first letter every board
+ function Message() {
+    // JSX: JavaScript XML
+    const name = 'Ambrus';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+
+
+ }
+ export default Message;
